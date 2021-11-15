7921e1645c115cf2bbdfb472619bf3ece6c79af9
Nikita Semenov <jgast88@gmail.com>
2021-11-15 20:08:48 +0300

    ADD validation overlap

--- app/models/department.rb
+++ app/models/department.rb
@@ -12,19 +12,21 @@ class Department < ApplicationRecord
 
 	# период существования 
 	def active_timerange
-			created_at..(disbanded_at || DateTime::Infinity.new)
+		created_at..(disbanded_at || DateTime::Infinity.new)
 	end
 
 	private
 
 	def overlaps_with_parent_department
-		if parent.present? and active_timerange.overlaps?(parent.active_timerange)
-			txt = %q(
-				период нахождения дочернего отдела в родительском должен лежать в
-				пределах периода существования родительского отдела (от даты
-				формирования до даты расформирования)
-			)
-			errors.add(:base, txt)
+		if parent
+			unless parent.active_timerange.cover?(active_timerange)
+				txt = %q(
+					период нахождения дочернего отдела в родительском должен лежать в
+					пределах периода существования родительского отдела (от даты
+					формирования до даты расформирования)
+				)
+				errors.add(:base, txt)
+			end
 		end
 	end
 end
